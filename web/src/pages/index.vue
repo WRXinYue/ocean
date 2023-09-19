3b16eb68102f5362813eb8d65a17e838b9a08a04
WRXinYue <3322543587@qq.com>
2023-09-19 16:35:57 +0800

    feat: 测试页面

--- web/src/pages/index.vue
+++ web/src/pages/index.vue
@@ -1,51 +1,46 @@
-<script setup lang="ts">
-defineOptions({
-  name: 'IndexPage',
-})
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
-</script>
-
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+  <div h-100vh w-full flex flex-col>
+    <!-- Head -->
+    <div h-14 w-full border />
+    <div h-full flex>
+      <!-- Nav Bar -->
+      <div h-full w-40 flex flex-col overflow-y-auto border-r>
+        <!-- Navigation Menu -->
+        <nav class="nav-menu" mt-4>
+          <ul>
+            <li class="nav-item" mb-2>
+              <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item" mb-2>
+              <a href="#" class="nav-link">About</a>
+            </li>
+            <li class="nav-item" mb-2>
+              <a href="#" class="nav-link">Services</a>
+            </li>
+            <li class="nav-item" mb-2>
+              <a href="#" class="nav-link">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <!-- Body Content -->
+      <div h-full w-full>
+        123
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.nav-link {
+  display: block;
+  padding: 8px 16px;
+  color: #495057;
+  text-decoration: none;
+}
+
+.nav-link:hover {
+  background-color: #f8f9fa;
+  color: #007bff;
+}
+</style>
