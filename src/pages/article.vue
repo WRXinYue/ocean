5238b61d9e812c3ccadd8747dd222f745f945cf5
WRXinYue <3322543587@qq.com>
2023-10-18 02:53:55 +0800

    feat: complete list read

--- src/pages/article.vue
+++ src/pages/article.vue
@@ -3,27 +3,42 @@ import { readBinaryFile, readDir } from '@tauri-apps/api/fs'
 import { POSITION, useToast } from 'vue-toastification'
 
 const toast = useToast()
+const metaDataList: Ref<any> = ref([])
 
 /** Get meta information */
-function mateFun(mdString: string) {
+function convertToDict(mdString: string) {
   const frontMatter = mdString.split('---')[1]
-  const metadata = {}
+  const dict = {} as any
   const lines = frontMatter.split('\n')
 
-  lines.forEach((line) => {
-    if (line) {
-      const parts = line.split(':')
-      const key = parts[0].trim()
-      let value = parts.slice(1).join('join').trim() as any
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim()
+    if (line.includes(':')) {
+      const splitLine = line.split(':')
+      const key = splitLine[0].trim()
+      const value = splitLine[1].trim()
 
-      if (value[0] === '-')
-        value = value.slice(1).trim().split('\n -').map((s) => { return s.trim })
-      metadata[key] = value
+      if (key === 'tags' || key === 'categories') {
+        // If it's a list item, keep reading lines until finished
+        dict[key] = []
+        for (let j = i + 1; j < lines.length; j++) {
+          if (lines[j].trim().startsWith('-')) {
+            dict[key].push(lines[j].trim().substring(1).trim())
+          }
+          else {
+            // If the next line does not start with a dash ('-'), it's not part of the list
+            i = j - 1
+            break
+          }
+        }
+      }
+      else {
+        dict[key] = value
+      }
     }
-  })
-  console.log(metadata)
+  }
 
-  return metadata
+  return dict
 }
 
 /** Get the title and summary of the article */
@@ -38,14 +53,15 @@ async function redFileTitle() {
         const contents = await readBinaryFile(entry.path)
         const decoder = new TextDecoder()
         const mdStr = decoder.decode(contents)
-        mateFun(mdStr)
-
+        const metaData = convertToDict(mdStr)
+        metaDataList.value.push(metaData)
         fileNames.push(entry.name)
       }
       else {
         console.warn('This is not an MD file')
       }
     }
+    console.log(metaDataList)
   }
   else {
     toast.warning('No folder path set in localStorage.', {
@@ -68,127 +84,16 @@ async function redFileTitle() {
   </n-button>
   <div h-full w-full>
     <n-list clickable hoverable>
-      <n-list-item>
-        <n-thing title="延迟加载策略" content-style="margin-top: 10px;">
+      <n-list-item v-for="metaData in metaDataList" :key="metaData.title">
+        <n-thing :title="metaData.title" content-style="margin-top: 10px;">
           <template #description>
             <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                性能优化
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                用户体验
+              <n-tag v-for="tag in metaData.tags" :key="tag" :bordered="false" type="info" size="small">
+                {{ tag }}
               </n-tag>
             </n-space>
           </template>
-          提高响应速度<br>
-          提升自身的开发素质
-        </n-thing>
-      </n-list-item>
-      <n-list-item>
-        <n-thing title="微服务架构" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                分布式系统
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                容器化
-              </n-tag>
-            </n-space>
-          </template>
-          构建高效的、可扩展的系统<br>
-          实现服务的高度自治<br>
-          早晨好，我的开发团队<br>
-          让我们一起构建庞大的工厂吧
-        </n-thing>
-      </n-list-item>
-    </n-list>
-    <n-list clickable hoverable>
-      <n-list-item>
-        <n-thing title="后端与数据库交互" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                Python
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                SQL
-              </n-tag>
-            </n-space>
-          </template>
-          以勇气和决心进行编程，<br>
-          实现你伟大的项目目标
-        </n-thing>
-      </n-list-item>
-      <n-list-item>
-        <n-thing title="前端JavaScript框架" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                React
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                Vue.js
-              </n-tag>
-            </n-space>
-          </template>
-          利用最新的框架技术<br>
-          构建绚丽的用户界面<br>
-          早安，我的编程伙伴，<br>
-          让我们一起打造出令人惊叹的产品
-        </n-thing>
-      </n-list-item>
-      <n-list-item>
-        <n-thing title="机器学习与深度学习" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                TensorFlow
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                PyTorch
-              </n-tag>
-            </n-space>
-          </template>
-          探索神经网络的神秘世界，<br>
-          创造出智能化的解决方案<br>
-          让机器学习助力人类前进
-        </n-thing>
-      </n-list-item>
-    </n-list>
-    <n-list clickable hoverable>
-      <n-list-item>
-        <n-thing title="晚来的启示" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                温暖的代码
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                春季代码重构
-              </n-tag>
-            </n-space>
-          </template>
-          勇敢地编码，然后好好休息<br>
-          这就是程序员的伟大生涯
-        </n-thing>
-      </n-list-item>
-      <n-list-item>
-        <n-thing title="他在朝未来的大门" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                循环递归
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                潜在的驱动程序
-              </n-tag>
-            </n-space>
-          </template>
-          那最新的打印机操纵逻辑<br>
-          为彩色接口复制着代码<br>
-          早安，我的定时任务先生<br>
-          让他带你走进硅川的工厂
+          {{ metaData.data }}
         </n-thing>
       </n-list-item>
     </n-list>
